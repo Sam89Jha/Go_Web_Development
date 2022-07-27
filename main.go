305fdac76ce032de1f99edd666301d7d4bc4218c
Sameer Kumar Jha <sameer.jha@grabtaxi.com>
2022-07-27 12:02:20 +0800

    Fixed issue

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"log"
 
+	restapi "github.com/Sam89Jha/Go_Web_Development/rest_api"
 	sqsutil "github.com/Sam89Jha/Go_Web_Development/sqs_util"
 )
 
 func main() {
+
+	fmt.Println("Welcome to Rest API example !!")
+	restapi.RegisterAPIs()
+
 	fmt.Println("Welcome to AWS SQS practice")
-	ctx := context.Background()
-	subscribe(ctx)
+	// ctx := context.Background()
+	// subscribe(ctx)
 	//publish(ctx)
 
 }
--- rest_api/books_api.go
+++ rest_api/books_api.go
@@ -45,14 +45,22 @@ func RegisterAPIs() {
 	fmt.Println("Hello world!")
 
 	//router
-	r := mux.NewRouter()
+	router := mux.NewRouter()
 
 	//Mock Data
 	books = append(books, Book{ID: "1", Isbn: "10000", Title: "Go Rest API", Author: &Author{FirstName: "Test", LastName: "First"}})
 	books = append(books, Book{ID: "2", Isbn: "10001", Title: "Book2", Author: &Author{FirstName: "Test", LastName: "Second"}})
 
 	//Route handlers
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+
+	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		tpl, err1 := route.GetPathTemplate()
+		met, err2 := route.GetMethods()
+		fmt.Println(tpl, err1, met, err2)
+		return nil
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
